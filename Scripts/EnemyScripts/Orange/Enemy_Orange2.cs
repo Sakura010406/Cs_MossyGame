184d8a4bfaa9ec916dd8eb72db3f8fed444a3270
Sakura010406 <79800321+Sakura010406@users.noreply.github.com>
2021-11-03 18:50:34 +0800

    Add files via upload

---
+++ Scripts/EnemyScripts/Orange/Enemy_Orange2.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Orange2 : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float waitTime;
+    [SerializeField]
+    private Transform[] movePos;
+
+    private int i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, movePos[i].position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, movePos[i].position) < 0.1f)
+        {
+            if (waitTime < 0.0f)
+            {
+                if (i == 0)
+                {
+                    i = 1;
+                }
+                else
+                {
+                    i = 0;
+                }
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
